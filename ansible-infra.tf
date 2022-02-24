5f257fc94d1e027fa47faa03189053789b8ce6a1
bathulaashwini02 <bathula.ashwini02@gmail.com>
2022-02-24 12:55:24 +0000

    terraform variables

--- ansible-infra.tf
+++ ansible-infra.tf
@@ -1,8 +1,12 @@
 # Configure the AWS Provider
+# Variables For ACK & SCK
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # VPC

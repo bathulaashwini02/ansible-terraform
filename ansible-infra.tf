4259802e7205d207cd65be4b18a3c79f626676f7
bathulaashwini02 <bathula.ashwini02@gmail.com>
2022-02-24 13:12:16 +0000

    dns

--- ansible-infra.tf
+++ ansible-infra.tf
@@ -13,7 +13,7 @@ provider "aws" {
 resource "aws_vpc" "ansible_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-
+  enable_dns_support = "true"
   tags = {
     Name = "ansible vpc"
   }
